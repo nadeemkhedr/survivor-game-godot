5e941fe9a2b0bb1ed1c6f5ebf7e54ec8097f6590
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-12 08:59:41 +0300

    ✨ feat: damage player on interval when touching enemies

--- scenes/game_object/game_camera/game_camera.gd
+++ scenes/game_object/game_camera/game_camera.gd
@@ -16,6 +16,6 @@ func _process(delta):
 
 func acquire_target():
 	var player_nodes = get_tree().get_nodes_in_group("player")
-	if player_nodes.size() >= 0:
+	if player_nodes.size() > 0:
 		var player = player_nodes[0] as Node2D
 		target_position = player.global_position
