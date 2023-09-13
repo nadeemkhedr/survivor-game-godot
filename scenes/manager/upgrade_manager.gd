d5d30ec9a518fabbeec63f1b691be5ad7652b914
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-13 15:30:22 +0300

    ✨ feat: axe can be selected once in the upgrades

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -20,13 +20,22 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+		
+	if upgrade.max_quantity >  0:
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity:
+			upgrade_pool = upgrade_pool.filter(func(pool_upgrade): return pool_upgrade.id != upgrade.id)
+	
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
 func pick_upgrades():
 	var filtered_upgrades = upgrade_pool.duplicate()
 	var chosen_upgrades: Array[AbilityUpgrade] = []
+	
 	for i in 2:
+		if filtered_upgrades.size() == 0:
+			break
 		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
 		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
 		chosen_upgrades.append(chosen_upgrade)
