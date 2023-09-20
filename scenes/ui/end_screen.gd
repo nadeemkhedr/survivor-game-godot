fdfc23aab2364ecc1cb3a211e565a41ab870f5d7
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-20 15:44:16 +0300

    ✨ feat: integrate upgrades in the main menu, vial drop upgrade works now

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -9,7 +9,7 @@ func _ready():
 	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3)\
 	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	get_tree().paused = true
-	$%RestartButton.pressed.connect(on_restart_button_pressed)
+	$%ContinueButton.pressed.connect(on_continue_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
 
 
@@ -18,18 +18,22 @@ func set_defeat():
 	$%DescriptionLabel.text = "You lost!"
 	play_jingle(true)
 
+
 func play_jingle(defeat: bool = false):
 	if defeat:
 		$DefeatStreamPlayer.play()
 	else:
 		$VictoryStreamPlayer.play()
 
-func on_restart_button_pressed():
+
+func on_continue_button_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/meta_menu.tscn")
 	
 	
 func on_quit_button_pressed():
-	get_tree().quit()
+	ScreenTransition.transition_to_scene("res://scenes/ui/main_menu.tscn")
+	await ScreenTransition.transitioned_halfway
+	get_tree().paused = false
