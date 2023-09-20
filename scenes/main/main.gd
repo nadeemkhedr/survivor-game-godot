f50f7d2d7ff604968ce03bca880ea70c78111f9e
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-20 10:54:38 +0300

    ✨ feat: add meta upgrade card

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -13,6 +13,7 @@ func _unhandled_input(event):
 		add_child(pause_menu_scene.instantiate())
 		get_tree().root.set_input_as_handled()
 
+
 func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
